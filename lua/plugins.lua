053ae149a4a35ea8730d67772893c462ae3f7b87
Rishabh Rathore <rishabhrathod20@gmail.com>
2023-12-18 16:15:58 +0530

    Stable again

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -13,40 +13,42 @@ vim.opt.rtp:prepend(lazypath)
 
 --The plugin list for the neovim
 local plugins = {
-     "dracula/vim",
-     --Telescope Plugin
-     {
-         "nvim-telescope/telescope.nvim", tag = '0.1.5',
-         dependencies = { 'nvim-lua/plenary.nvim'}
-     },
-     ---[[
-     {
-         "rose-pine/nvim",
-         as = 'rose-pine',
-         config = function()
-             vim.cmd('colorscheme rose-pine')
-         end
-     },
-     --]]
-     --Treesitter for the syntax highlighting
-     {
-         "nvim-treesitter/nvim-treesitter",
-         run = ':TSUpdate'
-     }, 
-     "nvim-treesitter/playground",
-     "theprimeagen/harpoon",
-     "mbbill/undotree",
-     "tpope/vim-fugitive", 
-     --THe lsp here 
-     ----- Uncomment these if you want to manage LSP servers from neovim
-     {'williamboman/mason.nvim'},
-     {'williamboman/mason-lspconfig.nvim'},
-     {'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
-     {'neovim/nvim-lspconfig'},
-     {'hrsh7th/cmp-nvim-lsp'},
-     {'hrsh7th/nvim-cmp'},
-     {'L3MON4D3/LuaSnip'},
-     
+    "dracula/vim",
+    { "catppuccin/nvim", name = "catppuccin" },
+    --"nvim-tree/nvim-web-devicons",
+    --Telescope Plugin
+    {
+        "nvim-telescope/telescope.nvim", tag = '0.1.5',
+        dependencies = { 'nvim-lua/plenary.nvim'}
+    },
+    ---[[
+    {
+        "rose-pine/nvim",
+        as = 'rose-pine',
+        config = function()
+            vim.cmd('colorscheme rose-pine')
+        end
+    },
+    --]]
+    --Treesitter for the syntax highlighting
+    {
+        "nvim-treesitter/nvim-treesitter",
+        run = ':TSUpdate'
+    }, 
+    "nvim-treesitter/playground",
+    "theprimeagen/harpoon",
+    "mbbill/undotree",
+    "tpope/vim-fugitive", 
+    --THe lsp here 
+    ----- Uncomment these if you want to manage LSP servers from neovim
+    {'williamboman/mason.nvim'},
+    {'williamboman/mason-lspconfig.nvim'},
+    {'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
+    {'neovim/nvim-lspconfig'},
+    {'hrsh7th/cmp-nvim-lsp'},
+    {'hrsh7th/nvim-cmp'},
+    {'L3MON4D3/LuaSnip'},
+
 }
 
 local opts = {}
