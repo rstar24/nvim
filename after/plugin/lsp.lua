2f119257d377536f46e26375fffee9563b1c0709
Rishabh Rathore <rishabhrathod20@gmail.com>
2023-12-18 15:40:49 +0530

    Added the lsp keybings

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -3,7 +3,32 @@
 lsp.on_attach(function(client, bufnr)
   -- see :help lsp-zero-keybindings
   -- to learn the available actions
-  lsp.default_keymaps({buffer = bufnr})
+  --lsp.default_keymaps({buffer = bufnr})
+  local opts = {buffer = bufnr, remap = false}
+  vim.opt.tabstop = 4
+  vim.opt.softtabstop = 4
+  vim.opt.shiftwidth = 4
+  vim.opt.expandtab = true
+
+  vim.opt.smartindent = true 
+  vim.opt.wrap = false
+
+  vim.opt.swapfile = false
+  vim.opt.backup = false
+  vim.opt.undodir = os.getenv("HOME").."/.vim/undodir"
+  vim.opt.undofile = true
+
+  vim.opt.hlsearch = false
+  vim.opt.incsearch = true
+  vim.opt.termguicolors = true
+
+  vim.opt.scrolloff = 8
+  vim.opt.signcolumn = yes
+  vim.opt.isfname:apped("@-@")
+
+  vim.opt.updatetime = 50
+  vim.opt.colorcolumn = "80"
+
 end)
 
 require('mason').setup({})
