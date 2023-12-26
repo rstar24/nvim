cdaa240ed953f5ebc11c37b9d0672fc661692079
Rishabh Rathore <rishabhrathod20@gmail.com>
2023-12-26 22:08:29 +0530

    added the vscode-like snippets

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -51,8 +51,9 @@ local plugins = {
     {'williamboman/mason-lspconfig.nvim'},
     {'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
     {'neovim/nvim-lspconfig'},
-    {'hrsh7th/cmp-nvim-lsp'},
+    {'hrsh7th/cmp-nvim-lsp'}, 
     {'hrsh7th/nvim-cmp'},
+    {'hrsh7th/cmp-path'},
     {'L3MON4D3/LuaSnip'},
 
     --For adding vs code like snippets
