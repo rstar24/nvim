a77f57ccc487ffd4fcd5f5a805cd44a5caf16a16
Rishabh Rathore <rishabhrathod20@gmail.com>
2024-01-19 12:12:20 +0530

    Reinstalling

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,10 +1,10 @@
 require'nvim-treesitter.configs'.setup {
     -- A list of parser names, or "all" (the five listed parsers should always be installed)
     -- I can add the languages I want to add for the syntax highlighting
-    ensure_installed = { "java","latex","rust","go","javascript","typescript","c","cpp", "lua", "vim", "vimdoc", "query" },
+    ensure_installed = { "markdown","markdown_inline","java","latex","rust","go","javascript","typescript","c","cpp", "lua", "vim", "vimdoc", "query" },
 
     -- Install parsers synchronously (only applied to `ensure_installed`)
-    sync_install = false,
+    sync_install = true,
 
     -- Automatically install missing parsers when entering buffer
     -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
@@ -17,6 +17,6 @@ require'nvim-treesitter.configs'.setup {
     -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
     -- Using this option may slow down your editor, and you may see some duplicate highlights.
     -- Instead of true it can also be a list of languages
-    additional_vim_regex_highlighting = false,
+    -- additional_vim_regex_highlighting = true,
     },
 }
