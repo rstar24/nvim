2be0cdeeffd3394ba414f9a261157b2f042695b3
rstar24 <rishabhrathod20@gmail.com>
2024-09-29 14:11:42 +0530

    Some keymaps and configuration

--- lua/core/remaps.lua
+++ lua/core/remaps.lua
@@ -4,6 +4,7 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 vim.keymap.set("n", "J", "mzJ`z")
@@ -19,6 +20,7 @@ vim.keymap.set("x", "<leader>p", [["_dP]])
 -- next greatest remap ever : asbjornHaland
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
+
 
 vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
 
@@ -37,4 +39,42 @@ vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
 vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>");
 
+-- making simple keymaps for compiling and executing 
+-- cpp and c programs 
+
+
+vim.keymap.set('n', '<F6>', '<ESC>:w<CR>:!g++ -fsanitize=address -std=c++17 -DONPC -O2 -o %< % && ./%<  < inp | lolcat <CR>', { noremap = true, silent = true })
+
+-- A remap for live grep using telescope
+vim.keymap.set('n', '<leader>lg', function()
+      require('telescope.builtin').live_grep({
+        search_dirs = {vim.fn.expand('%:p:h')},
+        file_ignore_patterns = {"*.o", "*.exe",".class"}
+      })
+    end, {
+      noremap = true,
+      silent = true,
+      desc = "Telescope live grep in current file directory"
+    }
+)
+
+-- Trying jj to Esc keymap
+vim.api.nvim_set_keymap('i', 'jj', '<Esc>', { noremap = true, silent = true })
+
+-- making indent line toggle keymap
+-- Function to toggle indent lines
+function toggle_indent_lines()
+    if vim.g.indent_blankline_enabled == nil then
+        vim.g.indent_blankline_enabled = true -- Set to true if it was never set
+    end
+    vim.g.indent_blankline_enabled = not vim.g.indent_blankline_enabled -- Toggle the state
+    if vim.g.indent_blankline_enabled then
+        print("Indent lines enabled")
+    else
+        print("Indent lines disabled")
+    end
+end
+
+-- Create a key mapping to toggle indent lines
+vim.api.nvim_set_keymap('n', '<leader>il', ':lua toggle_indent_lines()<CR>', { noremap = true, silent = true })
 
